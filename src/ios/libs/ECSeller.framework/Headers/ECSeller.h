4d3107e6aad2a5118c3701c1de222839dfec7b1b
Peter Liu <uzstudio@icloud.com>
2015-12-29 16:45:35 +0800

    ECSeller framework

---
+++ src/ios/libs/ECSeller.framework/Headers/ECSeller.h
@@ -0,0 +1,20 @@
+//
+//  ECSeller.h
+//  ECSeller
+//
+//  Created by 刘孛 on 15/12/29.
+//  Copyright © 2015年 pliu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ECSeller.
+FOUNDATION_EXPORT double ECSellerVersionNumber;
+
+//! Project version string for ECSeller.
+FOUNDATION_EXPORT const unsigned char ECSellerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ECSeller/PublicHeader.h>
+
+#import <ImageUpload/ImageUploader.h>
+
