50e647cd91b80d3e87350570da0b681e41b8ab0b
Peter Liu <uzstudio@icloud.com>
2015-12-29 17:12:54 +0800

    fix framework header file copy

---
+++ src/ios/libs/ECSeller.framework/Headers/ImageUploader.h
@@ -0,0 +1,15 @@
+//
+//  ImageUploader.h
+//  ImageUploader
+//
+//  Created by 刘孛 on 15/12/29.
+//  Copyright © 2015年 pliu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageUploader : NSObject
+
+- (NSString*)uploadImage:(NSString*)filePath;
+
+@end
